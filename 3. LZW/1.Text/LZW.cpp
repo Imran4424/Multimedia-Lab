32a5168506af91e5f45e575a99a765d172f72ab9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-02-05 17:05:14 +0600

    LZW

--- 3. LZW/1.Text/LZW.cpp
+++ 3. LZW/1.Text/LZW.cpp
@@ -4,7 +4,19 @@
 #include <map>
 using namespace std;
 
+typedef long long int lli;
 
+void compress()
+{
+	unordered_map < string, int > codes;
+
+	for(lli i = 0; i < 256; i++)
+	{
+		codes[string(1, char(i))] = i;
+	}
+
+	lli next_code = 257;
+}
 
 
 
