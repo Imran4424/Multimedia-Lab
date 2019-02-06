06bad39b9a88b22a878f34bb66cbe1701daa19a9
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-02-06 14:54:22 +0600

    lab exam

--- 1. Run length Coding/3. Exam/RLE.cpp
+++ 1. Run length Coding/3. Exam/RLE.cpp
@@ -2,11 +2,67 @@
 #include <fstream>
 using namespace std;
 
-typedef long long int lli;
-
 void Compress()
 {
-    
+	char c, old_c;
+	int count = 0;
+
+	ifstream readFile;
+
+	readFile.open("smallPlain.txt");
+
+	if(readFile.is_open())
+	{
+		
+		readFile >> noskipws >> c;
+
+		while(readFile)
+		{
+
+			old_c = c;
+
+			count = 1;
+
+			while(readFile >> noskipws >> c && old_c == c)
+			{
+				count++;
+			}
+
+			if (c == old_c && count == 1)
+			{
+				break;
+			}
+
+			ofstream writeFile;
+
+			writeFile.open("compress.txt", ios::app);
+
+			if (writeFile.is_open())
+			{
+				writeFile << int(old_c) << " " << count << endl;
+
+				writeFile.close();
+			}
+			else
+			{
+				cout << "can not open the file compress.txt" << endl;
+			}
+
+		}
+		
+		readFile.close();
+	}
+	else
+	{
+		cout << "can not open the file plain.txt" << endl;
+	}
+}
+
+
+
+void Decompress()
+{
+
 }
 
 
